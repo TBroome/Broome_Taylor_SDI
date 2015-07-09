4a95b6c629af97521ab53d9769ce692e4710b282
TBroome <Avariea@Taylors-MacBook-Pro.local>
2015-07-09 12:52:09 -0400

    Fish v2.0
    Final output

--- Broome_Taylor_Output_Assignment/Broome_Taylor_Variables.js
+++ Broome_Taylor_Output_Assignment/Broome_Taylor_Variables.js
@@ -1,10 +1,12 @@
-//Taylor Broome class section 02 Output Assignment//
+// Taylor Broome Scalable Data Infrastructures class section 02 Output Assignment
 
 var phrase = "These are my fish!"
 
-var fishTypes = 8;
+var fishTypes = 8; // number variable
 
-var hasFish = true;
+var hasFish = true; // boolean variable
 
-var fishTypes = "goldfish"; "molly"; "gobby"; "tetra"; "red fin shark";
+var fishTypes = "goldfish"; "molly"; "gobby"; "tetra"; "red fin shark"; // string variable
+
+
 
