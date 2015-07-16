5fa1a558ddfb0bf59e76006a4b6b6e7d5d43ed5a
TBroome <Avariea@Taylors-MacBook-Pro.local>
2015-07-16 16:21:03 -0400

    Prompts

--- Expressions Practice/js/script.js
+++ Expressions Practice/js/script.js
@@ -1,9 +1,4 @@
-//practice PEMDAS
+//practice Prompts
 
-var quiz1 = 87;
-var quiz2 = 99;
-var quiz3 = 88;
-var quiz4 = 79;
-
-var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
-console.log(average)
+var userInput = prompt("Enter your year of birth");
+console.log(userInput);
