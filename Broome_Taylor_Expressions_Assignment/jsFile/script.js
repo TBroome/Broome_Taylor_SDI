ae31b1c92c3cfd0ea881cc0edabbb23494c7eeb1
TBroome <Avariea@Taylors-MacBook-Pro.local>
2015-07-16 20:00:27 -0400

    Working

--- Broome_Taylor_Expressions_Assignment/jsFile/script.js
+++ Broome_Taylor_Expressions_Assignment/jsFile/script.js
@@ -1,5 +1,11 @@
 //Expressions
 
-alert("lets check that gear ilvl!");
-var helm = prompt(userInput)
-console.log(ilvl);
+alert("lets check that gear iLvl!");
+var helm = prompt("Enter your helm level here please. \nValue must be between 600 and 675.")
+var chest = prompt("Enter your chest level here please. \nValue must be between 600 and 675.")
+var legs = prompt("Enter your legs level here please. \nValue must be between 600 and 675.")
+var iLvl = ((helm + chest + legs)/4)
+alert("Great job! Here is your iLvl!")
+alert = iLvl
+
+console.log(iLvl);
