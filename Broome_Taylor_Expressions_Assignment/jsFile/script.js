35ffdb747ebf6dd4407132f0a7413e2294fdcfe3
TBroome <Avariea@Taylors-MacBook-Pro.local>
2015-07-16 20:02:53 -0400

    not adding properly…

--- Broome_Taylor_Expressions_Assignment/jsFile/script.js
+++ Broome_Taylor_Expressions_Assignment/jsFile/script.js
@@ -4,8 +4,8 @@ alert("lets check that gear iLvl!");
 var helm = prompt("Enter your helm level here please. \nValue must be between 600 and 675.")
 var chest = prompt("Enter your chest level here please. \nValue must be between 600 and 675.")
 var legs = prompt("Enter your legs level here please. \nValue must be between 600 and 675.")
-var iLvl = ((helm + chest + legs)/4)
+var iLvl = (helm + chest + legs) / 3
 alert("Great job! Here is your iLvl!")
-alert = iLvl
+alert(iLvl)
 
 console.log(iLvl);
