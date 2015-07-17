ad8966679657a6bc5b2b5c3d62410df94c73b1c3
TBroome <Avariea@Taylors-MacBook-Pro.local>
2015-07-16 20:39:22 -0400

    new try

--- Broome_Taylor_Expressions_Assignment/jsFile/script.js
+++ Broome_Taylor_Expressions_Assignment/jsFile/script.js
@@ -1,13 +1,16 @@
 //Expressions Taylor Broome 7/16/2015
 
-alert("lets check that gear iLvl!");
+alert("lets check that power level!");
 var helm = prompt("Enter your helm item level here please. \nEnter between 30 and 40 please.");
 var chest = prompt("Enter your chest item level here please. \nEnter between 1 and 10 please.");
 var legs = prompt("Enter your legs item level here please. \nEnter between 1 and 10 please.");
 var average = helm - chest - legs;
 var average = average*3;
-alert("Great job! Your iLvl is " + average);
+alert("Great job! Your power level is " + average);
 console.log(average);
 
-alert("OK Now lets see if you can fight that boss yeah?")
-var average = 
+alert("OK Now lets see if you can fight that boss yeah?");
+var average = [helm, chest, legs];
+var total = average[0] + average[1] + average[2];
+var iLvl = total/3;
+alert("You can go kick some boss butt with an item level of " + iLvl);
