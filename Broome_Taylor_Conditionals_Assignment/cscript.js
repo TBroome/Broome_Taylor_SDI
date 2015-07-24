87d47566090f4bb107de814b03308b6b4966a09a
TBroome <Avariea@Taylors-MacBook-Pro.local>
2015-07-24 13:07:11 -0400

    adding on comments

--- Broome_Taylor_Conditionals_Assignment/cscript.js
+++ Broome_Taylor_Conditionals_Assignment/cscript.js
@@ -4,17 +4,17 @@
 
 //sorry this is late I really did not get a chance to work ok it
 
-var fishFood = prompt("Please enter the amount of food pellets you have here.");
-var numFish = prompt("Please enter the number of fish you have here.");
-var days = prompt("Please enter the number of days you need to feed your fish.");
+var fishFood = prompt("Please enter the amount of food pellets you have here."); //prompting for the number of fish food pellets owned
+var numFish = prompt("Please enter the number of fish you have here.");     //prompting to get the number of fish owned
+var days = prompt("Please enter the number of days you need to feed your fish.");       //prompting to understand the number of days to feed fish
 
-if(fishFood < numFish*days){
-    console.log("You need more food.");
-}else{
-    console.log("You have enough food.");
+if(fishFood < numFish*days){                //first conditional to see if you have more food then fish
+    console.log("You need more food.");     //print out to show user if they need more food
+}else{                                      //if the above statement is false this is then displayed
+    console.log("You have enough food.");   //print out to show if you DO have enough food
 }
 
-var goldfish = false;
+var goldfish = false;                       //to see if you do or do not have goldfish
 var food = "Use Goldfish Pellets";
 var mollies = true;
 
