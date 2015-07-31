bbbe33c29cd6ac9838f51ad78fe95c175df04f47
TBroome <Avariea@Taylors-MacBook-Pro.local>
2015-07-30 20:31:50 -0400

    first try

--- Broome_Taylor_Functions_Assignment/Js File/scriptfile.js
+++ Broome_Taylor_Functions_Assignment/Js File/scriptfile.js
@@ -2,3 +2,26 @@
 //SDI 7/29/2015
 
 console.log("Test");
+
+var yes = prompt("Would you like the Powerball numbers?");
+
+if(yes){
+    console.log(numbers)
+}else{
+    console.log(lotto)
+}
+
+
+function powerball(a, b, c, d, e, f){
+    var numbers = (a "," b "," c "," d "," e "," f ".")
+    return numbers;  //function spitting info out
+}
+
+console.log(numbers);
+
+a(3);
+b(10);
+c(27);
+d(35);
+e(42);
+f(53);
